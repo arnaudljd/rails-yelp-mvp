a8e9b74ab48cc455b4bb1012a77802419a59029c
Arnaud Lejade <arnaud.lejade1@gmail.com>
2023-05-24 14:41:39 +0200

    Restaurant's portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.new(name:"Resto 1", address: "1 villa Gaudelet", category: "chinese")
+Restaurant.new(name:"Resto 2", address: "2 villa Gaudelet", category: "french")
+Restaurant.new(name:"Resto 3", address: "3 villa Gaudelet", category: "italian")
+Restaurant.new(name:"Resto 4", address: "4 villa Gaudelet", category: "japanese")
+Restaurant.new(name:"Resto 5", address: "5 villa Gaudelet", category: "belgian")
